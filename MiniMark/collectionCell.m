de89bb3ec14d04821667a4e16cabf313eba74304
尹尚维 <yinshangwei@gmail.com>
2016-08-05 16:02:56 +0800

    delete comments

--- MiniMark/collectionCell.m
+++ MiniMark/collectionCell.m
@@ -16,22 +16,7 @@
 - (instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
-//        //这里需要初始化ImageView；
-//        self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width - (UISCREEN_WIDTH - 40) / 6 - 20, self.frame.size.width-(UISCREEN_WIDTH - 40) / 6 - 20, 40, 40)];
-//        [self.imageView setUserInteractionEnabled:true];
-//        self.label = [[UILabel alloc] initWithFrame:CGRectMake(0, self.frame.size.width - (UISCREEN_WIDTH - 40) / 6 + 20, (UISCREEN_WIDTH - 40) / 3, 20)];
-//        self.label.textAlignment = NSTextAlignmentCenter;
-//        self.label.text = @"111";
-//        self.deleteBtn = [[UIButton alloc] initWithFrame:CGRectMake(CELL_WIDTH - 35, 5, 30, 30)];
-//        [self.deleteBtn setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
-//        //先设置不可见；
-//        [self.deleteBtn setHidden:true];
-//        
-//        self.layer.borderWidth = 0.5;
-//        
-//        [self addSubview:self.imageView];
-//        [self addSubview:self.label];
-//        [self addSubview:self.deleteBtn];
+        //这里需要初始化ImageView；
     }
     return self;
 }
